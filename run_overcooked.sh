e206c04bd2adbd29836d42cb6cb43c2ba9e1cd1b
Giulia <g.benintendi@studenti.unibs.it>
2026-03-23 10:41:23 +0100

    overcooked: only cramped room dual b=0.25 ent=0.45 feed_attn

--- run_overcooked.sh
+++ run_overcooked.sh
@@ -8,25 +8,6 @@ mkdir -p logs
 
 nohup bash -c "
 
-echo \"[\$(date +%H:%M)] coord_ring single b=0.001 ent=0.01 5s\"
-./run_gpu.sh $GPU marl.run \
-    -cn base_config_ja_ippo \
-    task=overcooked-v1/coord_ring \
-    algorithm=ja_ippo/overcooked-v1/coord_ring \
-    algorithm.NUM_SEEDS=5 \
-    algorithm.TOTAL_TIMESTEPS=5e6 \
-    algorithm.JA_BETA_MAX=0.001
-
-echo \"[\$(date +%H:%M)] coord_ring single b=0.001 ent=0.45 5s\"
-./run_gpu.sh $GPU marl.run \
-    -cn base_config_ja_ippo \
-    task=overcooked-v1/coord_ring \
-    algorithm=ja_ippo/overcooked-v1/coord_ring \
-    algorithm.NUM_SEEDS=5 \
-    algorithm.TOTAL_TIMESTEPS=5e6 \
-    algorithm.JA_BETA_MAX=0.001 \
-    algorithm.ENT_COEF=0.45
-
 echo \"[\$(date +%H:%M)] cramped_room dual b=0.25 ent=0.45 feed_attn 5s\"
 ./run_gpu.sh $GPU marl.run \
     -cn base_config_ja_ippo \
