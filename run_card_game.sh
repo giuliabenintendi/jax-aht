d3f7a655f79065aa19b15cfec0b602d94472555e
Giulia <g.benintendi@studenti.unibs.it>
2026-03-24 08:15:28 +0100

    run_card_game: feed_attn 5M 5 seeds, b=0.1, LR=8e-4

--- run_card_game.sh
+++ run_card_game.sh
@@ -8,22 +8,13 @@ mkdir -p logs
 
 nohup bash -c "
 
-echo \"[\$(date +%H:%M)] filter_cards, b=0.1, LR=8e-4, 5M, 5 seeds\"
+echo \"[\$(date +%H:%M)] feed_attn, b=0.1, LR=8e-4, 5M, 5 seeds\"
 ./run_gpu.sh $GPU marl.run \
     -cn base_config_ja_ippo task=card-game algorithm=ja_ippo/card-game \
     algorithm.NUM_SEEDS=5 algorithm.TOTAL_TIMESTEPS=5e6 \
     algorithm.FEED_OTHER_ATTN=true algorithm.JA_BETA_MAX=0.1 \
-    algorithm.FILTER_ATTN_CARDS=true algorithm.LR=8e-4 \
-    label=filter_cards_5M
-
-echo \"[\$(date +%H:%M)] fixed_partner0, b=0.1, LR=8e-4, 5M, 1 seed\"
-./run_gpu.sh $GPU marl.run \
-    -cn base_config_ja_ippo task=card-game algorithm=ja_ippo/card-game \
-    algorithm.NUM_SEEDS=1 algorithm.TOTAL_TIMESTEPS=5e6 \
-    algorithm.FEED_OTHER_ATTN=true algorithm.JA_BETA_MAX=0.1 \
     algorithm.LR=8e-4 \
-    task.ENV_KWARGS.fixed_partner_pos=0 \
-    label=fixed0_5M
+    label=feed_attn_5M
 
 echo \"[\$(date +%H:%M)] Done\"
 " > logs/card_game.log 2>&1 &
