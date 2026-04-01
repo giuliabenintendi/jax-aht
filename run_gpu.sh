1ae53704e5a27828cd62b366195c9b97f7d1171d
Giulia <g.benintendi@studenti.unibs.it>
2026-04-01 10:39:23 +0200

    revert run_gpu.sh to original

--- run_gpu.sh
+++ run_gpu.sh
@@ -7,7 +7,6 @@ shift 2>/dev/null
 
 CUDA_VISIBLE_DEVICES="${device}" \
 XLA_PYTHON_CLIENT_PREALLOCATE=false \
-XLA_FLAGS="--xla_gpu_strict_conv_algorithm_picker=false" \
 LD_LIBRARY_PATH="" \
 nice -n 5 \
 uv run python -m "$@"
