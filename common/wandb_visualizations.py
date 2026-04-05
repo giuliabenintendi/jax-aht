3257764fecf2a98f695bbda764e4e912b7c2fd86
Giulia <g.benintendi@studenti.unibs.it>
2026-04-05 23:25:21 +0200

    add cross_attn wandb tag

--- common/wandb_visualizations.py
+++ common/wandb_visualizations.py
@@ -105,6 +105,8 @@ def _build_tags(config) -> list[str]:
         tags.append("feed_attn")
     else:
         tags.append("no_feed_attn")
+    if alg_config.get("CROSS_AGENT_ATTN", False):
+        tags.append("cross_attn")
     if alg_config.get("FILTER_ATTN_TOP1", False):
         tags.append("top1")
     env_kwargs = alg_config.get("ENV_KWARGS", {})
